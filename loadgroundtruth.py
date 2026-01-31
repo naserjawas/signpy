dfedf759c92c0cbde24ff718ac16075e2e83ece8
Naser Jawas <naser.jawas@gmail.com>
2026-01-31 12:32:54 +0700

    change 0 and 1 to the sign id

--- loadgroundtruth.py
+++ loadgroundtruth.py
@@ -25,13 +25,7 @@ def load_gt(filename, videofile):
         if framedata[-3] == videofile:
             fn = framedata[-1].split("_")[-1]
             fnid = int(fn[2:8])
-            # convert to the segment ground truth
-            # 0: sign
-            # 1: silent/"si" (non-sign)
             # sign id for "si" is 3693
-            if signid == 3693:
-                gtdata.append([fnid, 1])
-            else:
-                gtdata.append([fnid, 0])
+            gtdata.append((fnid, signid))
 
     return gtdata
