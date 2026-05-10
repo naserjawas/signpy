bc127660476f4903c51f8d1ac01a1b4fb7effd8f
Naser Jawas <naser.jawas@gmail.com>
2026-05-10 10:00:26 +0700

    new training sign segment

--- trainsignsegment.py
+++ trainsignsegment.py
@@ -6,6 +6,10 @@ from classmodel.bilstm import BiLSTM
 import torch.optim as optim
 import torch.nn as nn
 import torch
+import os
+import glob
+
+from pathlib import Path
 
 
 def smoothness_loss(pred):
@@ -15,64 +19,65 @@ def smoothness_loss(pred):
     loss = (diff ** 2).mean()
     return loss
 
-device  = torch.device("mps" if torch.mps.is_available() else "cpu")
-# device  = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Device: {device}")
+def run_epoch(model, loader, criterion, optimiser, device,
+              training:bool, lambda_smooth):
+    model.train() if training else model.eval()
+    total_loss = 0.0
+    with torch.set_grad_enabled(training):
+        for x, y, lengths in loader:
+            x = x.to(device)
+            y = y.to(device)
 
-file_list = [
-    "../segprop/01April_2010_Thursday_heute_default-0_segprop.npz",
-    "../segprop/01April_2010_Thursday_heute_default-2_segprop.npz",
-    "../segprop/01April_2010_Thursday_heute_default-3_segprop.npz",
-    "../segprop/01April_2010_Thursday_heute_default-4_segprop.npz",
-    "../segprop/01April_2010_Thursday_heute_default-6_segprop.npz",
-    "../segprop/01April_2010_Thursday_heute_default-7_segprop.npz",
-    "../segprop/01April_2010_Thursday_heute_default-8_segprop.npz",
-]
+            pred = model(x)
+            bce = criterion(pred, y)
+            smooth = smoothness_loss(pred)
+            loss = bce + lambda_smooth * smooth
 
-dataset = SignSegmentDataset(file_list)
-criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([5.0]).to(device))
-loader = DataLoader(
-    dataset,
-    batch_size=4,
-    shuffle=True,
-    collate_fn=collate_fn
-)
+            if training:
+                optimiser.zero_grad()
+                loss.backward()
+                optimiser.step()
 
+            total_loss += loss.item()
+        avg_loss = total_loss / len(loader)
 
-model = TemporalCNN(input_dim=3, hidden_dim=64, num_layers=4)
-# model = BiLSTM(input_dim=3, hidden_dim=64, num_layers=1)
-model = model.to(device)
+    return avg_loss
 
-optimiser = optim.Adam(model.parameters(), lr=1e-4)
+def get_filelist(dirname):
+    dirpath = Path(dirname)
+    dirpath = str(dirpath) + os.sep
+    filenames = sorted(glob.glob(dirname + "*.npz"))
+    print(f"Get {len(filenames)} npz filenames... OK")
 
-num_epochs = 30
-lambda_smooth = 0.1
+    return filenames
 
-for epoch in range(num_epochs):
-    model.train()
-    total_loss = 0
-    for x, y, lengths in loader:
-        x = x.to(device)
-        y = y.to(device)
-        y = y.float()
+if __name__ == "__main__":
+    device  = torch.device("mps" if torch.mps.is_available() else "cpu")
+    # device  = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Device: {device}")
 
-        optimiser.zero_grad()
+    train_files = get_filelist("../segprop_train/")
+    train_dataset = SignSegmentDataset(train_files)
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=4,
+        shuffle=True,
+        collate_fn=collate_fn
+    )
+    print(f"DataLoader from {len(train_files)} files... OK")
 
-        pred = model(x)
-        bce = criterion(pred, y)
-        smooth = smoothness_loss(pred)
-        loss = bce + lambda_smooth * smooth
+    # model = TemporalCNN(input_dim=3, hidden_dim=64, num_layers=4)
+    model = BiLSTM(input_dim=3, hidden_dim=64, num_layers=1)
+    model = model.to(device)
 
-        loss.backward()
-        optimiser.step()
-        total_loss += loss.item()
+    criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([5.0]).to(device))
+    optimiser = optim.Adam(model.parameters(), lr=1e-6)
 
-        print(pred.min().item(), pred.max().item())
-        print(loss.item())
+    num_epochs = 30
+    lambda_smooth = 0.1
 
-    avg_loss = total_loss / len(loader)
-    print(f"Epoch {epoch}, Avg. Loss: {avg_loss:.4f}")
-
-    # model.eval()
-
-
+    print("Epoch started...")
+    for epoch in range(num_epochs):
+        avg_loss = run_epoch(model, train_loader, criterion, optimiser, device,
+                             True, lambda_smooth)
+        print(f"Epoch {epoch+1}: Training avg. loss: {avg_loss:.4f}")
